6778653592ce6ef9958e4044ffa0bcae1d65d71c
Devin Sova <devin.m.sova@gmail.com>
2024-03-17 19:50:31 -0700

    Basic gRPC Server Implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use proto::combo_server::{Combo, ComboServer};
+use tonic::transport::Server;
+
+mod proto {
+    tonic::include_proto!("combo");
+}
+
+#[derive(Debug, Default)]
+struct ComboService {}
+
+#[tonic::async_trait]
+impl Combo for ComboService {
+    async fn sol(
+        &self,
+        request: tonic::Request<proto::ComboRequest>,
+    ) -> Result<tonic::Response<proto::ComboResponse>, tonic::Status> {
+        println!("Got a request: {:?}", request);
+
+        let _input = request.get_ref(); //TODO: Actual Calculation
+
+        let response = proto::ComboResponse { damage: 420 };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+
+    let combo = ComboService::default();
+
+    Server::builder()
+        .add_service(ComboServer::new(combo))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
